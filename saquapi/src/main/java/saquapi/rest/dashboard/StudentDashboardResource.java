820dd0a12a96e1d430ad2dcb056a297e7586e166
Kovács Bence <kbence55@gmail.com>
2022-12-14 20:05:20 +0200

    Image upload base64 fixed

--- saquapi/src/main/java/saquapi/rest/dashboard/StudentDashboardResource.java
+++ saquapi/src/main/java/saquapi/rest/dashboard/StudentDashboardResource.java
@@ -33,6 +33,7 @@ public class StudentDashboardResource {
     @Path("/save")
     @POST
     public Response save(RequestDataRecord requestDataRecord) {
+        requestDataRecord.setBase64StringImage(requestDataRecord.getBase64StringImage().substring(23));
         dashboardService = (DashboardService) InvocationProxy.newInstance(new DashboardServiceBean());
         return Response.ok(new ResponseMessage(dashboardService.insertData(requestDataRecord) ? "SUCCESS" : "FAILED")).build();
     }
